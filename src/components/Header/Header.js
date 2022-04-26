b9ec81bb6fce22131a2efdf8da8b3a3b4539a3a2
hasanrafi69 <hasanrafi69@gmail.com>
2022-04-26 14:03:25 +0600

    fix routing load

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Container, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css'
 const Header = ({cart, wishlist}) => {
 	return (
 		<Navbar className='bg-custom' fixed='top' expand="lg">
 			<Container>
-				<Navbar.Brand href="/" className='text-white fs-4 fw-bold text-uppercase'>Store-House</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/" className='text-white fs-4 fw-bold text-uppercase'>Store-House</Navbar.Brand>
 				<Navbar.Toggle aria-controls="navbarScroll" className="fas fa-bars"/>
 				<Navbar.Collapse id="navbarScroll">
 				<Nav className="me-auto pe-3 my-2 my-lg-0">
-					<Nav.Link href="/">Home</Nav.Link>
-					<Nav.Link href="/question">Question</Nav.Link>
+					<Nav.Link as={Link} to="/">Home</Nav.Link>
+					<Nav.Link as={Link} to="/question">Question</Nav.Link>
 				</Nav>
 				<Form className="d-flex w-75">
 					<FormControl
